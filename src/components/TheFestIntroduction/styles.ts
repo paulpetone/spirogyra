1cee452ed3e520575c5de1db93c0fb814cff7998
paulpetone <paulohsilveira@outlook.com>
2023-02-23 19:01:16 -0300

    feat: TheFestIntroduction desktop style

--- src/components/TheFestIntroduction/styles.ts
+++ src/components/TheFestIntroduction/styles.ts
@@ -6,12 +6,25 @@ export default {
     display: flex;
     padding: 24px;
     align-items: center;
+
+    @media (min-width: 800px) {
+      padding: 56px 136px;
+
+      svg {
+        width: 136px;
+        height: 136px;
+      }
+    }
   `,
   Text: styled.p`
     margin-left: 2vw;
     max-width: 60vw;
     font-size: 14px;
     font-family: 'Scada-Regular';
+
+    @media (min-width: 800px) {
+      font-size: 24px;
+    }
 
     span {
       color: #AACF87;
