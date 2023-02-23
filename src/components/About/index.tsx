02ae90b6a37592cc46465d4669f459ecd2f44d5b
paulpetone <paulohsilveira@outlook.com>
2023-02-23 19:08:25 -0300

    refactor: fix text

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -9,9 +9,9 @@ export default function () {
         QUEM SOMOS
       </S.Title>
       <S.Green>Segundo o Wikipédia, </S.Green>
-      <S.Red>Spirogyra é um gênero de algas verdes, não ramificada, constituída por células cilíndricas que se ligam formando filamentos livre-flutuantes”.</S.Red>
+      <S.Red>Spirogyra "é um gênero de algas verdes, não ramificada, constituída por células cilíndricas que se ligam formando filamentos livre-flutuantes".</S.Red>
       <S.Green>Segundo Jorge Bem Jor, </S.Green>
-      <S.Red>Spirogyra é um bichinho bonito, verdinho, que dá na água”. </S.Red>
+      <S.Red>Spirogyra "é um bichinho bonito, verdinho, que dá na água". </S.Red>
       <S.Green>Segundo nós mesmos, </S.Green>
       <S.Red>Spirogyra (Spiro para os íntimos) é um convite à arte em sua mais alta heterogeneidade: um coletivo de músicos, tatuadores, artistas plásticos, designers, produtores, comunicadores e empreendedores, unidos com a intenção de enaltecer o talento, fortalecer a produção artística independente e facilitar o acesso à cena.</S.Red>
     </S.About>
