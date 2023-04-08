e6738634989d7a92e014fad2d47aee45eeb1780f
paulpetone <paulohsilveira@outlook.com>
2023-04-08 18:39:53 -0300

    feat: improve gallery

--- src/sections/Photos/index.tsx
+++ src/sections/Photos/index.tsx
@@ -1,19 +1,85 @@
-import { StaticImage } from 'gatsby-plugin-image'
-import React from 'react'
+import { Lightbox } from 'react-modal-image'
+import React, { useState } from 'react'
 import S from './styles'
+import cella from '../../images/photos/cella.jpg'
+import joaoecarol from '../../images/photos/joaoecarol.jpg'
+import mariebarbara from '../../images/photos/mariebarbara.jpg'
+import marienoedir from '../../images/photos/marienoedir.jpg'
+import spirogyras from '../../images/photos/spirogyras.jpg'
+import noedirecella from '../../images/photos/noedirecella.jpg'
+import paulo from '../../images/photos/paulo.jpg'
+import pauloemari from '../../images/photos/pauloemari.jpg'
+import rafa from '../../images/photos/rafa.jpg'
+
+const photos = {
+  cella: {
+    src: cella,
+    alt: 'Cella Azevedo'
+  },
+  joaoecarol: {
+    src: joaoecarol,
+    alt: 'João Nhoque e Carol'
+  },
+  mariebarbara: {
+    src: mariebarbara,
+    alt: 'Mariana Estol e Bárbara Pimentel'
+  },
+  marienoedir: {
+    src: marienoedir,
+    alt: 'Mariana Estol e Noedir'
+  },
+  spirogyras: {
+    src: spirogyras,
+    alt: 'Spirogyras'
+  },
+  noedirecella: {
+    src: noedirecella,
+    alt: 'Noedir e Cella Azevedo'
+  },
+  paulo: {
+    src: paulo,
+    alt: 'Paulpetone'
+  },
+  pauloemari: {
+    src: pauloemari,
+    alt: 'Paulpetone e Mariana Estol'
+  },
+  rafa: {
+    src: rafa,
+    alt: 'Rafa D\'Angelo'
+  }
+}
+
+type TPhoto = keyof typeof photos
 
 export default function () {
+  const [image, setImage] = useState(photos.cella)
+  const [modalIsVisible, setModalIsVisible] = useState(false)
+
+  const openModal = (image: string) => () => {
+    const currentImg = photos?.[image as TPhoto]
+    setImage(currentImg)
+    setModalIsVisible(true)
+  }
+  
   return (
     <S.Photos>
-      <StaticImage className='closed' alt="Cella" src="../../assets/photos/cella.jpg" />
-      <StaticImage className='closed' alt="João e Carol" src="../../assets/photos/joaoecarol.jpg" />
-      <StaticImage className='closed' alt="Mari e Ba" src="../../assets/photos/mariebarbara.jpg" />
-      <StaticImage className='closed' alt="Mari e Noedir" src="../../assets/photos/marienoedir.jpg" />
-      <StaticImage className='closed' alt="Spirogyras" src="../../assets/photos/spirogyras.jpg" />
-      <StaticImage className='closed' alt="Noedir e Cella" src="../../assets/photos/noedirecella.jpg" />
-      <StaticImage className='closed' alt="Paulo" src="../../assets/photos/paulo.jpg" />
-      <StaticImage className='closed' alt="Paulo e Mari" src="../../assets/photos/pauloemari.jpg" />
-      <StaticImage className='closed' alt="Rafa" src="../../assets/photos/rafa.jpg" />
+      {
+        modalIsVisible ?
+          <Lightbox
+            medium={image.src}
+            large={image.src}
+            alt={image.alt}
+            onClose={() => setModalIsVisible(false)} /> : null
+      }
+      {Object.keys(photos).map((value, key) => {
+        const currentPhoto = photos[value as TPhoto]
+        return (
+          <S.Photo key={`photo-${key}`} onClick={openModal(value)}>
+            <img alt={currentPhoto.alt} src={currentPhoto.src} />
+          </S.Photo>
+        )
+      })}
     </S.Photos>
   )
 }
--- src/sections/Photos/styles.ts
+++ src/sections/Photos/styles.ts
@@ -4,26 +4,29 @@ export default {
   Photos: styled.section`
     display: flex;
     flex-wrap: wrap;
-    
-    .gatsby-image-wrapper {
-      width: 33.3333%;
-      max-height: 120px;
-      cursor: pointer;
+  `,
+  Photo: styled.div`
+    width: 33.3333%;
+    overflow: hidden;
+    max-height: 120px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
 
-      @media (min-width: 800px) {
-        max-height: 426px;
-      }
+    @media (min-width: 800px) {
+      max-height: 426px;
+    }
 
-      &:hover {
-        img {
-          transform: scale(1.2);
-          filter: grayscale(0);
-        }
-      }
+    img {
+      width: 100%;
+      transition: all .2s linear;
+      filter: grayscale(1);
+    }
 
+    &:hover {
       img {
-        transition: all .2s linear;
-        filter: grayscale(1);
+        transform: scale(1.2);
+        filter: grayscale(0);
       }
     }
   `
