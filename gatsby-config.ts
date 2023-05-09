0b96e025ba428df74cf0c549d5081a75d9f8ab3d
paulpetone <paulohsilveira@outlook.com>
2023-05-08 22:37:28 -0300

    fix: favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -17,22 +17,6 @@ const config: GatsbyConfig = {
       }
     } 
   },
-  {
-    resolve: 'gatsby-plugin-favicons',
-    options: {
-      logo: './src/assets/favicon.ico',
-      appName: 'Spirogyra',
-      icons: {
-        android: true,
-        appleIcon: true,
-        appleStartup: true,
-        coast: false,
-        favicons: true,
-        yandex: false,
-        windows: false
-      }
-    }
-  },
   'gatsby-plugin-image',
   'gatsby-plugin-sharp',
   'gatsby-transformer-sharp']
